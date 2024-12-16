262b7cc66f0c35fd54d25556d94d017587d7b14a
Sepehr <sepehrfakoori@gmail.com>
2024-12-17 00:00:48 +0330

    feat(theme): add icons and create app icons class

--- lib/core/theme/app_icons.dart
+++ lib/core/theme/app_icons.dart
@@ -7,4 +7,12 @@ class AppIcons {
 
   // Dark theme icons
   static const darkAppLogo = '$_basePath/dark_logo.svg';
+
+  // Flags
+  static const englandFlag = '$_basePath/england_flag.svg';
+  static const franceFlag = '$_basePath/france_flag.svg';
+  static const germanyFlag = '$_basePath/germany_flag.svg';
+  static const uaeFlag = '$_basePath/uae_flag.svg';
+  static const turkeyFlag = '$_basePath/turkey_flag.svg';
+  static const iranFlag = '$_basePath/iran_flag.svg';
 }
