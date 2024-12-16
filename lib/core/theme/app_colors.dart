a1e045c49858b9401c56975e5719733d461497ed
Sepehr <sepehrfakoori@gmail.com>
2024-12-16 13:30:50 +0330

    feat(theme): Add theme/color/icons config

---
+++ lib/core/theme/app_colors.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  // Light Theme Colors
+  static const lightPrimaryColor = Color(0xff0EADD7);
+  static const lightDefaultBorderColor = Color(0xffEDEDED);
+  static const lightSeparatorColor = Color(0xffE8E8E8);
+  static const lightBackgroundPrimaryColor = Color(0xffF5F7FA);
+  static const lightBackgroundSecondaryColor = Color(0xffFFFFFF);
+  static const lightTitleColor = Color(0xff353535);
+  static const lightSubtitleColor = Color(0xff818181);
+  static const lightWhiteTextColor = Color(0xffFFFFFF);
+  static const lightBlueTextColor = Color(0xff0EADD7);
+  static const lightDeActiveButtonColor = Color(0xffEBEBEB);
+  static const lightWarningColor = Color(0xffE5B800);
+  static const lightBlue08Color = Color(0xffEBF7FD);
+
+  // Dark Theme Colors
+  static const darkPrimaryColor = Color(0xff7FE4F9);
+  static const darkDefaultBorderColor = Color(0xff3F3F3F);
+  static const darkSeparatorColor = Color(0xff3F3F3F);
+  static const darkBackgroundPrimaryColor = Color(0xff1D1D1D);
+  static const darkBackgroundSecondaryColor = Color(0xff262626);
+  static const darkTitleColor = Color(0xffFFFFFF);
+  static const darkSubtitleColor = Color(0xffD1D1D1);
+  static const darkWhiteTextColor = Color(0xffFFFFFF);
+  static const darkBlueTextColor = Color(0xff7FE4F9);
+  static const darkDeActiveButtonColor = Color(0xff3F3F3F);
+  static const darkWarningColor = Color(0xffE5B800);
+  static final darkBlue08Color = const Color(0xff0EADD7).withOpacity(12);
+}
---
+++ lib/core/theme/app_themes.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppThemes {
+  // Theme
+  static ThemeData themeData = ThemeData();
+
+  // Light Theme
+  static ThemeData lightThemeData = themeData.copyWith();
+
+  // Dark Theme
+  static ThemeData darkThemeData = themeData.copyWith();
+}
